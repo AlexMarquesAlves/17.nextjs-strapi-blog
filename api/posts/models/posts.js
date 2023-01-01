3f143131ade82dd52e118c431ff5aac408448205
¡Duque <atanaelleonardo@gmail.com>
2023-01-01 17:56:02 -0300

    𝑪𝒐𝒏𝒇𝒊𝒈𝒖𝒓𝒊𝒏𝒈 𝑺𝒕𝒓𝒂𝒑𝒊 𝒕𝒐 𝒏𝒐𝒕𝒊𝒇𝒚 𝑽𝒆𝒓𝒄𝒆𝒍

--- api/posts/models/posts.js
+++ api/posts/models/posts.js
@@ -1,8 +1,22 @@
-'use strict';
+"use strict";
+
+const axios = require("axios");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  async afterCreate(result, data) {
+    axios.post(
+      "https://api.vercel.com/v1/integrations/deploy/prj_os6LpEOFFZ62raj1frgJLnHCOqRX/5fJ9lhpYaD"
+    );
+  },
+
+  async afterUpdate(result, params, data) {
+    axios.post(
+      "https://api.vercel.com/v1/integrations/deploy/prj_os6LpEOFFZ62raj1frgJLnHCOqRX/5fJ9lhpYaD"
+    );
+  },
+};
